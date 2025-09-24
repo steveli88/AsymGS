accb5f9fe618913f158674c6b7969ba7cbdc3f29
Steve Li <steveli89@outlook.com>
2025-09-24 08:42:24 -0400

    modify script path

--- scripts/dataset_preparation.sh
+++ scripts/dataset_preparation.sh
@@ -7,19 +7,19 @@ python onthego_preprocess.py --input dataset/on-the-go/spot --output dataset/on-
 python onthego_preprocess.py --input dataset/on-the-go/patio_high --output dataset/on-the-go/patio_high-undistorted --size 504
 
 # RobustNeRF dataset
-colmap image_undistorter --image_path dataset/android/images --input_path dataset/android/sparse/0 --output_path dataset/android-undistorted --output_type COLMAP --max_image_size 504
-cp dataset/android/train_list.txt dataset/android-undistorted/train_list.txt
-cp dataset/android/test_list.txt dataset/android-undistorted/test_list.txt
+colmap image_undistorter --image_path dataset/robustnerf/android/images --input_path dataset/robustnerf/android/sparse/0 --output_path dataset/robustnerf/android-undistorted --output_type COLMAP --max_image_size 504
+cp dataset/robustnerf/android/train_list.txt dataset/robustnerf/android-undistorted/train_list.txt
+cp dataset/robustnerf/android/test_list.txt dataset/robustnerf/android-undistorted/test_list.txt
 
-colmap image_undistorter --image_path dataset/crab2/images --input_path dataset/crab2/sparse/0 --output_path dataset/crab2-undistorted --output_type COLMAP --max_image_size 432
-cp dataset/crab2/train_list.txt dataset/crab2-undistorted/train_list.txt
-cp dataset/crab2/test_list.txt dataset/crab2-undistorted/test_list.txt
+colmap image_undistorter --image_path dataset/robustnerf/crab2/images --input_path dataset/robustnerf/crab2/sparse/0 --output_path dataset/robustnerf/crab2-undistorted --output_type COLMAP --max_image_size 432
+cp dataset/robustnerf/crab2/train_list.txt dataset/robustnerf/crab2-undistorted/train_list.txt
+cp dataset/robustnerf/crab2/test_list.txt dataset/robustnerf/crab2-undistorted/test_list.txt
 
-colmap image_undistorter --image_path dataset/statue/images --input_path dataset/statue/sparse/0 --output_path dataset/statue-undistorted --output_type COLMAP --max_image_size 504
-cp dataset/statue/train_list.txt dataset/statue-undistorted/train_list.txt
-cp dataset/statue/test_list.txt dataset/statue-undistorted/test_list.txt
+colmap image_undistorter --image_path dataset/robustnerf/statue/images --input_path dataset/robustnerf/statue/sparse/0 --output_path dataset/robustnerf/statue-undistorted --output_type COLMAP --max_image_size 504
+cp dataset/robustnerf/statue/train_list.txt dataset/robustnerf/statue-undistorted/train_list.txt
+cp dataset/robustnerf/statue/test_list.txt dataset/robustnerf/statue-undistorted/test_list.txt
 
-colmap image_undistorter --image_path dataset/yoda/images --input_path dataset/yoda/sparse/0 --output_path dataset/yoda-undistorted --output_type COLMAP --max_image_size 432
-cp dataset/yoda/train_list.txt dataset/yoda-undistorted/train_list.txt
-cp dataset/yoda/test_list.txt dataset/yoda-undistorted/test_list.txt
+colmap image_undistorter --image_path dataset/robustnerf/yoda/images --input_path dataset/robustnerf/yoda/sparse/0 --output_path dataset/robustnerf/yoda-undistorted --output_type COLMAP --max_image_size 432
+cp dataset/robustnerf/yoda/train_list.txt dataset/robustnerf/yoda-undistorted/train_list.txt
+cp dataset/robustnerf/yoda/test_list.txt dataset/robustnerf/yoda-undistorted/test_list.txt
 
